a2989e2506617c3000c603bbf6019f835c876441
Vu Van Xung <xungvu2k1@gmail.com>
2024-01-22 10:14:15 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TabLayout_vs_ViewPager_vs_Fragment"
+include(":app")
+ 
